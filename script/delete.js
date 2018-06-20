48dafddde0591a1e4e785dffe50d53411d481045
cjcoder87 <c.jackson629.gmail.com>
2018-06-19 22:11:20 -0400

    all functions work

--- script/delete.js
+++ script/delete.js
@@ -18,8 +18,80 @@ $(document).ready(function(){
       document.querySelector('#deleteAnswer').value = JsAnswer[k];
       console.log('js html');
     }
-  
+
   });
-  
+
+  $("#deleteButton").click(function(){
+    var index = HtmlQuestion.indexOf(i);
+    var index2 = HtmlAnswer.indexOf(i);
+    var index3 = CssQuestion.indexOf(j);
+    var index4 = CssAnswer.indexOf(j);
+    var index5 = CssQuestion.indexOf(k);
+    var index6 = CssAnswer.indexOf(k);
+    
+
+    if($("#htmllink").hasClass("active")){
+    
+      if(i<=0){
+        document.querySelector('#deleteSpan').innerHTML = "You can't delete the last HTML index Card";
+        document.querySelector('#deleteSpan').style.display = "block";
+      }else{
+        HtmlQuestion.splice(index, 1);
+        HtmlAnswer.splice(index2, 1);
+        document.querySelector('#deleteSpan').innerHTML = "Succesfully Deleted";
+        document.querySelector('#deleteSpan').style.display = "block";
+        
+          document.querySelector('#deleteQuestion').value = ""; 
+          document.querySelector('#deleteAnswer').value = "";
+          i--;
+          document.querySelector('#card1').innerHTML = HtmlQuestion[i];
+          document.querySelector('.flashcard2 p').innerHTML = HtmlAnswer[i];
+          console.log('HTML delete accomplished');
+          
+      }
+
+}
+    
+    if($("#csslink").hasClass("active")){
+      if(j<=0){
+        document.querySelector('#deleteSpan').innerHTML = "You can't delete the last CSS index Card";
+        document.querySelector('#deleteSpan').style.display = "block";
+      }else{
+        CSSQuestion.splice(index3, 1);
+        CSSAnswer.splice(index3, 1);
+        document.querySelector('#deleteSpan').innerHTML = "Succesfully Deleted";
+        document.querySelector('#deleteSpan').style.display = "block";
+        
+          document.querySelector('#deleteQuestion').value = ""; 
+          document.querySelector('#deleteAnswer').value = "";
+          j--;
+          document.querySelector('#card1').innerHTML = CSSQuestion[j];
+          document.querySelector('.flashcard2 p').innerHTML = CSSAnswer[j];
+          console.log('Css delete accomplished');
+          
+      }
+    }
+    
+      if($("#jslink").hasClass("active")){
+        if(k<=0){
+          document.querySelector('#deleteSpan').innerHTML = "You can't delete the last JS index Card";
+          document.querySelector('#deleteSpan').style.display = "block";
+        }else{
+          JsQuestion.splice(index5, 1);
+          JsAnswer.splice(index6, 1);
+          document.querySelector('#deleteSpan').innerHTML = "Succesfully Deleted";
+          document.querySelector('#deleteSpan').style.display = "block";
+          
+            document.querySelector('#deleteQuestion').value = ""; 
+            document.querySelector('#deleteAnswer').value = "";
+            k--;
+            document.querySelector('#card1').innerHTML = JsQuestion[k];
+            document.querySelector('.flashcard2 p').innerHTML = JsAnswer[k];
+            console.log('JS delete accomplished');
+      }
+
+    }
+
+    });
   });
   // End of Document Ready
